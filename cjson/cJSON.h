d7ee5e70cd21f04111cf0e16563e3f741a3cd0d7
dylan7474 <112658365+dylan7474@users.noreply.github.com>
2025-08-29 22:57:03 +0100

    Add files via upload

--- cjson/cJSON.h
+++ cjson/cJSON.h
@@ -23,36 +23,6 @@
 #ifndef cJSON__h
 #define cJSON__h
 
-djones@hpebook:~/temp/closestPlane/cjson$ ls -l
-total 104
--rw-rw-r-- 1 djones djones 16394 Aug 29 21:21 cJSON.h
--rw-rw-r-- 1 djones djones 85088 Aug 29 22:49 cJSON.o
-djones@hpebook:~/temp/closestPlane/cjson$ cat cJSON.h 
-/*
-  Copyright (c) 2009-2017 Dave Gamble and cJSON contributors
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-*/
-
-#ifndef cJSON__h
-#define cJSON__h
-
 #ifdef __cplusplus
 extern "C"
 {
